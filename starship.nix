4388e352160e87ea56c75072085c81e019bdbe6c
Nicu Reut <nicu.reut@digitalasset.com>
2024-04-05 12:19:48 +0200

    Update starship.nix

--- starship.nix
+++ starship.nix
@@ -2,6 +2,8 @@
   programs.starship = {
     enable = true;
     settings = {
+      add_newline = true;
+      command_timeout = 3000;
       username = {
         style_user = "blue bold";
         style_root = "red bold";
